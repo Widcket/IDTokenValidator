fd0534ba4ab78c5df2833eb77dd509a543186e28
Rita Zerrizuela <zeta@widcket.com>
2019-09-12 23:21:14 -0300

    Implement validations

--- IDTokenValidator/IDTokenValidator.swift
+++ IDTokenValidator/IDTokenValidator.swift
@@ -15,17 +15,10 @@ public class IDTokenValidator {
      
      */
     public static func validate(_ idToken: String?, bundle: Bundle = Bundle.main) -> Bool {
-        // 1. clientID == aud
-        // 2. domain host == iss
-        // 2. sub split by pipe begins with "auth0" and ends in a valid uuid
-        // 3. exp not expired yet
+        // Check if the token is nil or malformed
+        guard let idToken = idToken, let claims = Claims(idToken) else { return false }
         
-        // Token is nil or malformed
-        guard let idToken = idToken, let claims = Claims(idToken) else {
-            return false
-        }
-        
-        guard let credentials = Credentials(bundle: bundle) else {
+        guard let credentials = Credentials(bundle) else {
             fatalError("Could not find Auth0.plist")
         }
         
@@ -37,23 +30,53 @@ private struct Claims {
     let iss: String
     let aud: String
     let sub: String
-    let exp: String
+    let exp: Double
     
     init?(_ idToken: String) {
-        self.iss = ""
-        self.aud = ""
-        self.sub = ""
-        self.exp = ""
+        // From https://github.com/auth0/Auth0.swift/blob/master/Auth0/OAuth2Grant.swift
+        
+        let splitToken = idToken.split(separator: ".")
+        
+        guard splitToken.count == 3 else { return nil }
+        
+        var base64 = splitToken[1]
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        
+        let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
+        let requiredLength = 4 * ceil(length / 4.0)
+        let paddingLength = Int(requiredLength - length)
+        
+        if paddingLength > 0 {
+            base64 += "".padding(toLength: paddingLength, withPad: "=", startingAt: 0)
+        }
+        
+        guard let data = Data(base64Encoded: base64, options: .ignoreUnknownCharacters),
+            let body = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+            let iss = body["iss"] as? String,
+            let aud = body["aud"] as? String,
+            let sub = body["sub"] as? String,
+            let exp = body["exp"] as? Double else { return nil }
+        
+        self.iss = iss
+        self.aud = aud
+        self.sub = sub
+        self.exp = exp
     }
 }
 
 private struct Credentials {
     let clientId: String
-    let domain: URL
+    let domain: String
     
-    init?(bundle: Bundle) {
-        self.clientId = ""
-        self.domain = URL(string: "https://widcket.auth0.com")!
+    init?(_ bundle: Bundle) {
+        guard let path = bundle.path(forResource: "Auth0", ofType: "plist"),
+            let credentials = NSDictionary(contentsOfFile: path),
+            let clientId = credentials["ClientId"] as? String,
+            let domain = credentials["Domain"] as? String else { return nil }
+ 
+        self.clientId = clientId
+        self.domain = domain
     }
 }
 
@@ -65,19 +88,31 @@ private class ClaimsValidator {
     }
     
     private func validateIss(_ iss: String) -> Bool {
-        return true
+        guard let url = URL(string: iss), let host = url.host else { return false }
+        
+        return host == credentials.domain
     }
     
     private func validateAud(_ aud: String) -> Bool {
-        return true
+        return aud == credentials.clientId
     }
     
     private func validateSub(_ sub: String) -> Bool {
-        return true
+        let splitSub = sub.split(separator: "|")
+        
+        guard let auth0 = splitSub.first, let userId = splitSub.last else { return false }
+        
+        let alphanumericCharset = CharacterSet.decimalDigits.union(CharacterSet.lowercaseLetters)
+        let userIdCharset = CharacterSet(charactersIn: String(userId.lowercased()))
+        
+        return auth0 == "auth0" &&
+            !userId.isEmpty &&
+            alphanumericCharset.isSuperset(of: userIdCharset)
     }
     
-    private func validateExp(_ exp: String) -> Bool {
-        return true
+    private func validateExp(_ exp: Double) -> Bool {
+        // 5 minutes of leeway for server time
+        return Date(timeIntervalSince1970: exp) > Date().addingTimeInterval(5.0 * 60.0)
     }
     
     func validate(_ claims: Claims) -> Bool {
